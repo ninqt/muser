c845d3479eeabe384d29eb14d8029cd25bd4d70b
Sean Grainger <ninqt@protonmail.com>
2026-05-25 19:04:11 +1000

    Changed navbar name, added logo and button styling

--- Muser/frontend/src/components/Navbar.jsx
+++ Muser/frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import logo from '../assets/logo_white.svg';
 
 const Navbar = () => {
   const { user, logout } = useAuth();
@@ -11,8 +12,10 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <Link to="/" className="text-2xl font-bold">Task Manager</Link>
+    <nav className="bg-[#9B0F0F] text-white p-4 flex justify-between items-center">
+      <Link to="/" className="text-3xl font-bold">
+	  <img src={logo} alt="Muser Logo" className="inline-flex w-10 h-10 -translate-y-0.5"/>
+	  Muser</Link>
       <div>
         {user ? (
           <>
@@ -27,10 +30,10 @@ const Navbar = () => {
           </>
         ) : (
           <>
-            <Link to="/login" className="mr-4">Login</Link>
+            <Link to="/login" className="bg-[#9B0F0F] font-bold ring-2 ring-white ring-inset px-4 py-2 rounded mr-4 hover:underline">Login</Link>
             <Link
               to="/register"
-              className="bg-green-500 px-4 py-2 rounded hover:bg-green-700"
+              className="bg-white text-[#9B0F0F] font-bold px-4 py-2 rounded hover:underline"
             >
               Register
             </Link>
