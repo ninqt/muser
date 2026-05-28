0db8ab3aeb264c8e4a1b510e35533efa95d63ec2
Sean Grainger <ninqt@protonmail.com>
2026-05-28 19:31:21 +1000

    Added admin flag to user creation

--- Muser/backend/models/User.js
+++ Muser/backend/models/User.js
@@ -6,8 +6,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    university: { type: String },
-    address: { type: String },
+    admin: { type:Boolean, default: false, required:true}
 });
 
 userSchema.pre('save', async function (next) {
